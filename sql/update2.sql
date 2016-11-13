d08acd9195a5b4e9d532bea5ac3309a5d20ce914
Richard Hughes <richard@rhconsultingllc.com>
2016-11-12 18:43:36 -0600

    smartgroups - sql update2

--- sql/update2.sql
+++ sql/update2.sql
@@ -5,7 +5,7 @@ ALTER TABLE smart_groups add google_domain_id int(10) not null default 0 after s
 DROP TABLE IF EXISTS `google_domains`;
 CREATE TABLE IF NOT EXISTS `google_domains` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `domain_name` varchar(255) default NULL,
+  `name` varchar(255) default NULL,
   `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `deleted_at` timestamp NULL DEFAULT NULL,
